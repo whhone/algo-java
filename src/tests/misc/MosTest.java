b9e9f1576cc28fde42709fca6c38190024600e09
Wai Hon Law <whhone@gmail.com>
2016-02-02 14:17:48 +0800

    Add MosTest for Mos.

---
+++ src/tests/misc/MosTest.java
@@ -0,0 +1,65 @@
+package tests.misc;
+
+import org.junit.Test;
+import weapon.misc.Mos;
+
+import java.util.Random;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MosTest {
+
+  @Test
+  public void test() {
+    int sizeOfArray = 1000;
+    int numOfQuery = 1000;
+
+    final int[] a = new int[sizeOfArray];
+
+    Mos mos = new Mos(sizeOfArray, numOfQuery) {
+
+      @Override
+      public void rightRight() {
+        curRight++;
+        curAns += a[curRight];
+      }
+
+      @Override
+      public void rightLeft() {
+        curAns -= a[curRight];
+        --curRight;
+      }
+
+      @Override
+      public void leftRight() {
+        curAns -= a[curLeft];
+        ++curLeft;
+      }
+
+      @Override
+      public void leftLeft() {
+        curLeft--;
+        curAns += a[curLeft];
+      }
+    };
+
+    for (int i = 0; i < sizeOfArray; i++) {
+      a[i] = i;
+    }
+
+    Random random = new Random();
+    int[] expectedAns = new int[numOfQuery];
+    for (int i = 0; i < numOfQuery; i++) {
+      int left = random.nextInt(sizeOfArray);
+      int right = left + random.nextInt(sizeOfArray - left);
+      mos.setQuery(i, left, right);
+      expectedAns[i] = right * (right + 1) / 2 - (left - 1) * (left) / 2;
+    }
+
+    mos.exec();
+
+    for (int i = 0; i < numOfQuery; i++) {
+      assertEquals(expectedAns[i], mos.getAnswer(i));
+    }
+  }
+}
