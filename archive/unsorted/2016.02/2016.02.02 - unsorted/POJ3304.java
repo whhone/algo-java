58030465e8035ea3a54abce3979971b7cbd87f1d
Wai Hon Law <whhone@gmail.com>
2016-02-15 14:00:58 +0800

    Strongly Connected Component with Kosaraju's algorithm.

---
+++ archive/unsorted/2016.02/2016.02.02 - unsorted/POJ3304.java
@@ -0,0 +1,12 @@
+package workspace;
+
+import io.InputReader;
+import io.OutputWriter;
+import weapon.graph.MinimumSpinningTree;
+
+public class POJ3304 {
+  public void solve(int testNumber, InputReader in, OutputWriter out) {
+    MinimumSpinningTree mst = new MinimumSpinningTree(3);
+    mst.solve();
+  }
+}
