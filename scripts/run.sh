80be87bd2d84a0a8541b1781343207b36ff94ee8
Wai Hon Law <whhone@gmail.com>
2017-04-24 23:24:41 -0700

    Run run.sh everywhere.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-cd output
-javac Main.java && java Main
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${ROOT_DIR}"
+cd ../output
+
+echo "${ROOT_DIR}"
+
+echo "Running javac Main.java"
+javac Main.java
+
+if [[ $? != 0 ]]; then
+    echo "Failed to build."
+else
+    java Main
+fi
 
